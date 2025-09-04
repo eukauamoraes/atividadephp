472bd12bca28829e30ab34d7a950d15a9afdd7f6
Kaua <kauamoraes270@gmail.com>
2025-09-04 11:41:46 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+// exercicio 1
+// $nome = "kauã Moraes";
+// echo "$nome"
+?>
+ 
+<?php 
+// exerciocio 2
+//  $idade = 19;
+// echo "eu tenho $idade anos"
+
+?>
+
+<?php 
+//exercico 3
+
+// $numero1 = 10;
+// $numero2 = 5;
+
+// echo -$nuemro1. "<br>";
+// echo +$numero2. "<br>";
+// echo "$numero1 + $numero2 = " .$numero1+$numero2 ; 
+?>
+
+<?php 
+//exercicio 4
+
+$cidade = "São paulo";
+$Pais = "Brasil";
+
+echo "Eu moro em $cidade,$Pais"
+
+?>
+
+
+
+
+
